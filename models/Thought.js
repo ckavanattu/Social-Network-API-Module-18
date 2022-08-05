614a7b7b62fad6851df656b7aa8ea91747bbb629
ckavanattu <chackojk@gmail.com>
2022-08-05 16:30:16 -0700

    added date formater

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 
 const ReactionSchema = new Schema ({
@@ -19,6 +20,7 @@ const ReactionSchema = new Schema ({
     createdAt: {
         type: Date,
         default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
     }
 });
 
@@ -33,6 +35,7 @@ const ThoughtSchema = new Schema (
         createdAt: {
             type: Date,
             default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
         },
         username:{
             type: String,
